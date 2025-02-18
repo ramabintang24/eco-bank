6010f1dab228c279f571c02ace57143bd3054c0d
23weqsszdzgv <tegr12q@gmail.com>
2025-02-18 08:06:58 +0700

    feat get wallet

---
+++ src/transaction/transaction.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Wallet } from './entities/wallet.entity';
+
+@Injectable()
+export class WalletService {
+    constructor (
+     @InjectRepository(Wallet) 
+     private Walletrepository: Repository<Wallet>,
+    ) {}
+
+    async getWalletByUserId(userId:number):Promise<Wallet> {
+        const Wallet = await this.Walletrepository.findOne({where:{userId} });
+        if (!wallet) {
+          throw new NotFoundException('wallet not ')
+        }
+    }
+}
