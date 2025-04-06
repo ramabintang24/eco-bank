85d73f71b7a499cf1df9e99c8f332daeb6146d02
ramabintang24 <ramaudaa24@gmail.com>
2025-04-06 19:48:56 +0700

    connect db

--- ecobank_db_migrations/data-source.ts
+++ ecobank_db_migrations/data-source.ts
@@ -1,20 +1,18 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   schema: 'public',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/ecobank_db_migrations/migrations/*.js'],
   synchronize: false,
+  ssl: {
+    rejectUnauthorized: false, // untuk Railway
+  },
 };
 
 const dataSource = new DataSource(dataSourceOptions);
